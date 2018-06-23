79fee9aed777846e1eda44b1b172fbd0cbeaba6a
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2018-06-23 15:07:30 +0545

    add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { Todo,User } = require('./models');
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//POST method for todos
 app.post('/todos', (req, res) => {
   const newTodo = new Todo({
     text: req.body.data
@@ -20,12 +22,31 @@ app.post('/todos', (req, res) => {
   
 })
 
+//GET method for todos
 app.get("/todos", (req, res) => {
   Todo.find()
     .then(todos => res.send({ todos }))
     .catch(e => res.send(e))
 })
 
+// fetch todos with id 
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return console.log('Id not valid');
+  }
+
+  Todo.findById(id)
+    .then(todos => {
+      if (!todos) {
+        return res.status(404).send()
+      }
+      res.send({todos})
+    })
+    .catch(e=> res.status(400).send())
+})
+
+//POST method for users
 app.post("/user", (req, res) => {
   const newUser = new User({
     email: req.body.user
